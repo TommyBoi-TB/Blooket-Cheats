bc8c34edaa52fdddb7393a489fc9480fa27b9533
Aerell <aerellmck@gmail.com>
2022-10-09 16:45:23 -0500

    Fix auto buy and sell

--- unobfuscated/global/autoSell.js
+++ unobfuscated/global/autoSell.js
@@ -3,12 +3,12 @@ document.body.append(i);
 window.alert = i.contentWindow.alert.bind(window);
 window.confirm = i.contentWindow.confirm.bind(window);
 i.remove();
-var axios = Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']],]).cache).find((x) => x.exports?.a?.get).exports.a;
-axios.get("https://api.blooket.com/api/users").then(async ({ data: { name, unlocks } }) => {
+var axios = Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']],]).cache).filter((x) => x.exports?.a?.get)[3].exports.a;
+axios.get("/api/users").then(async ({ data: { name, unlocks } }) => {
     let blooks = Object.entries(unlocks).filter(x => x[1] > 1);
     if (confirm(`Are you sure you want to sell your dupes?`)) {
         let now = Date.now();
-        for (const [blook, amount] of blooks) await axios.put("https://api.blooket.com/api/users/sellblook", { name, blook, numSold: amount - 1 });
+        for (const [blook, amount] of blooks) await axios.put("/api/users/sellblook", { name, blook, numSold: amount - 1 });
         alert(`(${Date.now() - now}ms) Results:\n${blooks.map(([blook, amount]) => `    ${blook} ${amount - 1}`).join(`\n`)}`);
     }
 }).catch(() => alert('There was an error user data!'));
