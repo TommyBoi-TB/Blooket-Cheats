b5f628a2e07a297cc0d3b305df93b03eee7a78ec
Aerell <aerellmck@gmail.com>
2022-10-10 15:53:19 -0500

    Update unobfuscated files

--- unobfuscated/global/getRewards.js
+++ unobfuscated/global/getRewards.js
@@ -5,6 +5,8 @@ i.remove();
 var axios = Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']],]).cache).filter((x) => x.exports?.a?.get)[3].exports.a;
 axios.get("/api/users").then(({ data: { name } }) => {
     axios.get("/api/users/bonuses").then(({ data: { tokensAvailable: addedTokens, xpAvailable: addedXp } }) => {
-        axios.put("/api/users/add-rewards", { name, addedTokens, addedXp });
-    }).then(() => alert(`Added ${addedTokens} tokens and ${addedXp} xp!`)).catch(() => alert('There was an error when adding rewards!'));
+        axios.put("/api/users/add-rewards", { name, addedTokens, addedXp })
+            .then(() => alert(`Added ${addedTokens} tokens and ${addedXp} xp!`))
+            .catch(() => alert('There was an error when adding rewards!'));
+    });
 }).catch(() => alert('There was an error user data!'));
