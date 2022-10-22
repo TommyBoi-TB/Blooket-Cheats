56e27a106a603866d3174e3e205a167837210144
Aerell <aerellmck@gmail.com>
2022-10-22 09:01:16 -0500

    Fix get rewards

--- unobfuscated/global/getRewards.js
+++ unobfuscated/global/getRewards.js
@@ -2,10 +2,10 @@ let i = document.createElement('iframe');
 document.body.append(i);
 window.alert = i.contentWindow.alert.bind(window);
 i.remove();
-var axios = Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']],]).cache).filter((x) => x.exports?.a?.get)[3].exports.a;
-axios.get("/api/users").then(({ data: { name } }) => {
-    axios.get("/api/users/bonuses").then(({ data: { tokensAvailable: addedTokens, xpAvailable: addedXp } }) => {
-        axios.put("/api/users/add-rewards", { name, addedTokens, addedXp })
+var axios = Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']],]).cache).find((x) => x.exports?.a?.get).exports.a;
+axios.get("https://dashboard.blooket.com/api/users").then(({ data: { name } }) => {
+    axios.get("https://play.blooket.com/api/users/bonuses").then(({ data: { tokensAvailable: addedTokens, xpAvailable: addedXp } }) => {
+        axios.put("https://play.blooket.com/api/users/add-rewards", { name, addedTokens, addedXp })
             .then(() => alert(`Added ${addedTokens} tokens and ${addedXp} xp!`))
             .catch(() => alert('There was an error when adding rewards!'));
     });
