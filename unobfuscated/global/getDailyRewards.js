d2b3c7a51e1e0db039e0c94c80483cbaf6a489db
Aerell <aerellmck@gmail.com>
2022-11-28 23:04:26 -0600

    Revert change

--- unobfuscated/global/getDailyRewards.js
+++ unobfuscated/global/getDailyRewards.js
@@ -5,9 +5,11 @@ i.remove();
 if (!location.href.includes("play.blooket.com")) alert("This cheat only works on play.blooket.com");
 else {
     var axios = Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']],]).cache).find((x) => x.exports?.a?.get).exports.a;
-    axios.get("https://dashboard.blooket.com/api/users").then(({ data: { name, tokensAvailable: addedTokens, xpAvailable: addedXp } }) => {
-        axios.put("https://play.blooket.com/api/users/add-rewards", { name, addedTokens, addedXp })
-            .then(() => alert(`Added ${addedTokens} tokens and ${addedXp} xp!`))
-            .catch(() => alert('There was an error when adding rewards.'));
+    axios.get("https://dashboard.blooket.com/api/users").then(({ data: { name } }) => {
+        axios.get("https://play.blooket.com/api/users/bonuses").then(({ data: { tokensAvailable: addedTokens, xpAvailable: addedXp } }) => {
+            axios.put("https://play.blooket.com/api/users/add-rewards", { name, addedTokens, addedXp })
+                .then(() => alert(`Added ${addedTokens} tokens and ${addedXp} xp!`))
+                .catch(() => alert('There was an error when adding rewards.'));
+        }).catch(() => alert("There was an error getting bonus data."));
     }).catch(() => alert('There was an error user data.'));
 }
