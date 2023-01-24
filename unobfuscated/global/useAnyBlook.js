d682240ca4fe7355ad333d74840868d3a6ccdaff
Aerell <aerellmck@gmail.com>
2023-01-24 15:54:50 -0600

    Fix thing

--- unobfuscated/global/useAnyBlook.js
+++ unobfuscated/global/useAnyBlook.js
@@ -23,8 +23,9 @@
             /* Update Checker end */
             const { stateNode } = Object.values(document.querySelector('#app > div > div'))[1].children[0]._owner;
             const blooks = webpackJsonp.push([[], { ['1234']: (_, a, b) => { a.webpack = b } }, [['1234']]]).webpack("MDrD").a;
-            if (location.pathname == "/blooks") stateNode.setState({ blookData: Object.keys(blooks).reduce((a, b) => (a[b] = (stateNode.state.blookData[b] || 1), a), {}), allSets: Object.values(blooks).reduce((a, b) => (a.includes(b.set) ? a : a.concat(b.set)), [])  });
-            else stateNode.setState({ unlocks: Object.keys(blooks) });
+            if (location.pathname == "/blooks") stateNode.setState({ blookData: Object.keys(blooks).reduce((a, b) => (a[b] = (stateNode.state.blookData[b] || 1), a), {}), allSets: Object.values(blooks).reduce((a, b) => (a.includes(b.set) ? a : a.concat(b.set)), []) });
+            else if (Array.isArray(stateNode.state.unlocks)) stateNode.setState({ unlocks: Object.keys(blooks) });
+            else stateNode.setState({ unlocks: blooks });
         }
     });
 })();
