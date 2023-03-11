6e3b28edacbd35a8b64d5dc94cd8e5ea2810c660
Aerell <aerellmck@gmail.com>
2023-03-10 19:44:56 -0600

    add 100ms delay to flood game

--- unobfuscated/gui.js
+++ unobfuscated/gui.js
@@ -19,7 +19,7 @@
     window.confirm = i.contentWindow.confirm.bind(window);
     i.remove();
     Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']]]).cache).find(x => x.exports?.a?.get).exports.a.get("https://" + (location.host.startsWith("dashboard") ? location.host : "play.blooket.com") + "/api/games?gameId=6368436a976422d8a3f70cd7").then(x => parseInt(`0${x.data.questions.find(x => x.question == "../cheats/gui.js")?.answers?.[0]}`)).then(async x => {
-        if (1678491467028 > x || confirm("This cheat is outdated and might be bugged, would you still like to run it? You can find regularly updated cheats here https://github.com/Minesraft2/Blooket-Cheats")) {
+        if (1678499072168 > x || confirm("This cheat is outdated and might be bugged, would you still like to run it? You can find regularly updated cheats here https://github.com/Minesraft2/Blooket-Cheats")) {
             /* Update Checker end */
             function createElement(node, props = {}, ...children) {
                 const element = document.createElement(node);
@@ -598,25 +598,28 @@
                             const axios = Object.values(webpack.c).find((x) => x.exports?.a?.get).exports.a;
                             const firebase = Object.values(webpack.c).find(x => x.exports?.a?.initializeApp).exports.a;
             
-                            for (let i = 1; i <= amount; i++) (async () => {
-                                const { data: { success, fbToken, fbShardURL } } = await axios.put("https://fb.blooket.com/c/firebase/join", { id, name: `${name}${i}` });
-                                if (!success) return;
-                                const liveApp = firebase.initializeApp({
-                                    apiKey: "AIzaSyCA-cTOnX19f6LFnDVVsHXya3k6ByP_MnU",
-                                    authDomain: "blooket-2020.firebaseapp.com",
-                                    projectId: "blooket-2020",
-                                    storageBucket: "blooket-2020.appspot.com",
-                                    messagingSenderId: "741533559105",
-                                    appId: "1:741533559105:web:b8cbb10e6123f2913519c0",
-                                    measurementId: "G-S3H5NGN10Z",
-                                    databaseURL: fbShardURL
-                                }, `${name}${i}`);
-                                const auth = firebase.auth(liveApp);
-                                await auth.setPersistence(firebase.auth.Auth.Persistence.NONE).catch(console.error);
-                                await auth.signInWithCustomToken(fbToken).catch(console.error);
-                                await liveApp.database().ref(`${id}/c/${name}${i}`).set({ b: "Black" });
-                                liveApp.delete();
-                            })();
+                            for (let i = 1; i <= amount; i++) {
+                                (async () => {
+                                    const { data: { success, fbToken, fbShardURL } } = await axios.put("https://fb.blooket.com/c/firebase/join", { id, name: `${name}${i}` });
+                                    if (!success) return;
+                                    const liveApp = firebase.initializeApp({
+                                        apiKey: "AIzaSyCA-cTOnX19f6LFnDVVsHXya3k6ByP_MnU",
+                                        authDomain: "blooket-2020.firebaseapp.com",
+                                        projectId: "blooket-2020",
+                                        storageBucket: "blooket-2020.appspot.com",
+                                        messagingSenderId: "741533559105",
+                                        appId: "1:741533559105:web:b8cbb10e6123f2913519c0",
+                                        measurementId: "G-S3H5NGN10Z",
+                                        databaseURL: fbShardURL
+                                    }, `${name}${i}`);
+                                    const auth = firebase.auth(liveApp);
+                                    await auth.setPersistence(firebase.auth.Auth.Persistence.NONE).catch(console.error);
+                                    await auth.signInWithCustomToken(fbToken).catch(console.error);
+                                    await liveApp.database().ref(`${id}/c/${name}${i}`).set({ b: "Black" });
+                                    liveApp.delete();
+                                })();
+                                await new Promise(r => setTimeout(r, 100));
+                            }
                         }
                     },
                     {
