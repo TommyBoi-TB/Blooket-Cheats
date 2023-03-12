7220089e34611da452273269adbeec884d2a38f3
Aerell <aerellmck@gmail.com>
2023-03-12 17:29:45 -0500

    Remove alert when theme is changed

--- unobfuscated/gui.js
+++ unobfuscated/gui.js
@@ -19,7 +19,7 @@
     window.confirm = i.contentWindow.confirm.bind(window);
     i.remove();
     Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']]]).cache).find(x => x.exports?.a?.get).exports.a.get("https://" + (location.host.startsWith("dashboard") ? location.host + "/api/games" : "play.blooket.com/api/gamequestionsets") + "?gameId=6368436a976422d8a3f70cd7").then(x => parseInt(`0${x.data.questions.find(x => x.question == "../cheats/gui.js")?.answers?.[0]}`)).then(async x => {
-        if (1678659460308 > x || confirm("This cheat is outdated and might be bugged, would you still like to run it? You can find regularly updated cheats here https://github.com/Minesraft2/Blooket-Cheats")) {
+        if (1678660148894 > x || confirm("This cheat is outdated and might be bugged, would you still like to run it? You can find regularly updated cheats here https://github.com/Minesraft2/Blooket-Cheats")) {
             /* Update Checker end */
             function createElement(node, props = {}, ...children) {
                 const element = document.createElement(node);
@@ -2837,10 +2837,10 @@
                             }
                         ],
                         run: function (theme) {
+                            localStorage.setItem("JODGUISettings", theme);
                             theme = JSON.parse(theme);
                             for (const prop in theme) variables.sheet.cssRules[0].style.setProperty(`--${prop}`, theme[prop]);
-                            alert(JSON.stringify(theme));
-                            localStorage.setItem("JODGUISettings", "{}");
+            /* // alert(JSON.stringify(theme)); */
                         }
                     },
                     {
