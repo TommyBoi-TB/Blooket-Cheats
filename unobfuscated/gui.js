4b699ffecabe96a18f243550edff08bc4dd062d6
Aerell <aerellmck@gmail.com>
2023-04-20 18:31:17 -0500

    Can't fix auto sell on open :C

--- unobfuscated/gui.js
+++ unobfuscated/gui.js
@@ -19,7 +19,7 @@
     window.confirm = i.contentWindow.confirm.bind(window);
     i.remove();
     Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']]]).cache).find(x => x.exports?.a?.get).exports.a.get("https://" + (location.host.startsWith("dashboard") ? location.host + "/api/games" : "play.blooket.com/api/gamequestionsets") + "?gameId=6368436a976422d8a3f70cd7").then(x => parseInt(`0${x.data.questions.find(x => x.question == "../cheats/gui.js")?.answers?.[0]}`)).then(async x => {
-        if (1682030447222 > x || confirm("This cheat is outdated and might be bugged, would you still like to run it? You can find regularly updated cheats here https://github.com/Minesraft2/Blooket-Cheats")) {
+        if (1682033454102 > x || confirm("This cheat is outdated and might be bugged, would you still like to run it? You can find regularly updated cheats here https://github.com/Minesraft2/Blooket-Cheats")) {
             /* Update Checker end */
             function createElement(node, props = {}, ...children) {
                 const element = document.createElement(node);
@@ -568,38 +568,6 @@
                                 this.enabled = false;
                                 clearInterval(this.data);
                                 this.data = null;
-                            }
-                        }
-                    },
-                    {
-                        name: "Auto Sell Dupes On Open",
-                        description: "Sells the blook you unlock when you open a pack if you already have one (Doesn't sell Legendaries or rarer)",
-                        type: "toggle",
-                        enabled: false,
-                        data: null,
-                        run: function () {
-                            if (!this.enabled) {
-                                this.enabled = true;
-                                this.data = XMLHttpRequest.prototype.open;
-                                XMLHttpRequest.prototype._original_open ||= XMLHttpRequest.prototype.open;
-                                XMLHttpRequest.prototype.open = function () {
-                                    this.addEventListener("load", async function () {
-                                        try {
-                                            if (this.responseURL !== "https://dashboard.blooket.com/api/users/unlockblook") return;
-                                            var axios = Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']],]).cache).find((x) => x.exports?.a?.get).exports.a;
-                                            var { rarity } = webpackJsonp.push([[], { ['rarity']: (_, a, b) => { a.rarity = (blook) => b('MDrD').a[blook].rarity } }, [['rarity']]]);
-                                            const { unlockedBlook: blook } = JSON.parse(this.response);
-                                            const { data: { name, unlocks } } = await axios.get("https://dashboard.blooket.com/api/users");
-                                            if (!unlocks[blook] || ["Legendary", "Chroma", "Mystical"].includes(rarity(blook))) return;
-                                            await axios.put("https://dashboard.blooket.com/api/users/sellblook", { name, blook, numSold: 1 });
-                                            console.log(`Sold duplicate ${blook}`);
-                                        } catch { }
-                                    });
-                                    XMLHttpRequest.prototype._original_open.apply(this, arguments);
-                                }
-                            } else {
-                                this.enabled = false;
-                                XMLHttpRequest.prototype.open = this.data;
                             }
                         }
                     },
